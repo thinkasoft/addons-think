87150250866e1ad8c031b684545b7fb2c13b9f9e
Hector Aular <aular.hector3@gmail.com>
2015-08-26 01:29:10 -0430

    modificando reporte 'registro contribucion'

--- hr_payslip_extended/wizard/hr_payslip_extended.py
+++ hr_payslip_extended/wizard/hr_payslip_extended.py
@@ -8,6 +8,7 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+import pdb
 
 
 class hr_payslip_extended(osv.osv):
@@ -16,10 +17,22 @@ class hr_payslip_extended(osv.osv):
     _columns = {
         'period_id': fields.many2one(
             'account.period', 'Period', readonly=False,
-            help="Period when the accounts entries were done"
-        ),
+            help="Period when the accounts entries were done"),
     }
     _defaults = {
     }
 
+    def on_change_method(self, cr, uid, period_id, context=None):
+        context = {}
+
+        obj_hp = self.pool.get('hr.payslip')
+        obj_hp_line = self.pool.get('hr.payslip.line')
+        lista = obj_hp.search(
+            cr, uid,
+            [("period_id", "=", period_id)],
+            order="employee_id", context=context
+        )
+        brw = obj_hp_line.browse(cr, uid, lista, context=context)
+
+        return {}
 hr_payslip_extended()
