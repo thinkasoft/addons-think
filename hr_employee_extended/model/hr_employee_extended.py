e45a056edb75701e91fba76d16870371325f1eb9
Hector Aular <aular.hector3@gmail.com>
2015-11-05 11:44:01 -0430

    Update module

--- hr_employee_extended/model/hr_employee_extended.py
+++ hr_employee_extended/model/hr_employee_extended.py
@@ -22,7 +22,7 @@ class Payroll_extension(osv.Model):
         'vat': fields.char('Vat', size=12, required=True),
         'withholding': fields.float('Income Withholding', size=5, digits=(2, 2), required=True),
         'salary': fields.float('Monthly salary', required=True),
-        'admission_date': fields.date('Date Admission', required=True),
+        'admission_date': fields.date('Date Admission', required=False),
         'date_exit': fields.date('Date Diacharge', required=False),
         'employment_benefit': fields.boolean('Employment Benefit?'),
         'reference_bank': fields.char('Reference Bank', size=50),
@@ -32,5 +32,4 @@ class Payroll_extension(osv.Model):
         'withholding': 0,
         'salary': 0,
         'vat': ' ',
-        'admission_date': fields.date.context_today,
     }
