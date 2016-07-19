3095a8e5e5dbdd27a6e5be6ebec402ecaa5aac3f
haular20 <aular.hector3@gmail.com>
2016-07-19 17:43:12 +0000

    flake8 ok

--- account_invoice_extended/models/account_invoice_extended.py
+++ account_invoice_extended/models/account_invoice_extended.py
@@ -37,7 +37,7 @@ class Account_invoice_extended (osv.Model):
     _inherit = 'account.invoice'
 
     def onchange_reference(self, cr, uid, ids, supplier_invoice_number,
-            context=None):
+                           context=None):
         """
         Gets through of parameters the supplier_invoice_number value and insert
         into reference field
