6b262c7a069b64b109279a85c1f29379bbde26f2
haular20 <aular.hector3@gmail.com>
2016-08-18 15:14:57 +0000

    add report for invetran

---
+++ account_invoice_report_invetran/report/account_print_invoiceInvetran.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Aular Hector Manuel <aular.hector3@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import time
+from openerp.report import report_sxw
+
+
+class account_invoice(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context):
+        super(account_invoice, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+        })
+report_sxw.report_sxw(
+    'report.account.invoice',
+    'account.invoice',
+    'account_invoice_report_invetran/report/report_invoice_invetran.rml',
+    parser=account_invoice
+)
