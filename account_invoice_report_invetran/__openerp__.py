caa3aeb303de8d4c1b0f0d36799c64d72b13b46b
haular20 <aular.hector3@gmail.com>
2016-08-18 13:56:32 +0000

    Create module account_invoice_report_invetran

---
+++ account_invoice_report_invetran/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Hector Manuel Aular <aular.hector3@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Invoice Invetran',
+    'version': '1',
+    'category': 'account',
+    'sequence': 1,
+    'author': 'Ingenieria Thinkasoft de Venezuela',
+    'website': 'http://www.thinkasoft.com',
+    'summary': """
+
+    """,
+    'description': """
+
+    """,
+    'depends': [
+        'account',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
