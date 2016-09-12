98d6120d3f5e5ecb5dc047fe233a744e1f0a7c40
haular20 <aular.hector3@gmail.com>
2016-09-12 12:23:34 +0000

    add traslate report

--- account_invoice_report_invetran/report/account_print_invoiceInvetran.py
+++ account_invoice_report_invetran/report/account_print_invoiceInvetran.py
@@ -9,6 +9,7 @@ from openerp.report import report_sxw
 class account_invoice(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context):
+        """ Init object for the report report_invoice_invetran """
         super(account_invoice, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
