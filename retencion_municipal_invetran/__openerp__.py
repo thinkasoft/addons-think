d470d01ce43eb689b09b6388f7c6baddf710c8ca
haular20 <aular.hector3@gmail.com>
2016-09-13 12:54:35 +0000

    change format Retencion municipal

---
+++ retencion_municipal_invetran/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Aular Hector Aular <aular.hector3@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Retencion municipal invetran",
+    'category': "Purchases",
+    'version': "1.0",
+    'depends': [
+        "purchase",
+        "l10n_ve_withholding_muni",
+    ],
+    'author': "Ingenieria Thinkasoft de Venezuela",
+    'website': 'http://www.thinkasoft.com',
+    'data': [
+        "views/report.xml",
+    ],
+    'installable': True,
+}
