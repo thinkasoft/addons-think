36ca80aed0db48b3027da58418457ba88051ddb7
Hector Manuel Aular Osorio <hector@debian>
2016-10-11 11:08:29 +0100

    módulo para instalar todas las dependencias

---
+++ module_config/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Héctor Manuel Aular Osorio <aular.hector3@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Configuracion OVL",
+    "summary": "Módulo para instalar toda las dependencias de la OVL",
+    "version": "10.0",
+    "category": "Config",
+    "website": "http://www.thinkasoft.com",
+    "author": "Héctor Manuel Aular Osorio, Ingenieria Thinkasoft de Venezuela",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends":
+    [
+        "base",
+        "account",
+        "hr",
+        "hr_contract",
+        "sale",
+        "purchase",
+        "fleet",
+    ],
+}
